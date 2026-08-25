4ad8d1e6159255124bc1602c0702e37be26b622b
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-25 20:42:07 -0300

    Add program to find predecessor and successor of a number

---
+++ exercicio03.py
@@ -0,0 +1,10 @@
+''' Antecessor e sucessor '''
+
+numero = int(input("Digite um número inteiro: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print ("antecessor:, antecessor")
+print ("numero:, numero")
+print ("sucessor:, sucessor")
