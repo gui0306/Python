e0a8da5bc2b48faf47c99e4fce0a41fdc6d8855d
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-25 20:51:57 -0300

    Add program to calculate double, triple, and half

---
+++ exercicio04.py
@@ -0,0 +1,12 @@
+''' Dobro, triplo e metade '''
+
+numero = int(input("Digite um número inteiro: "))
+
+numeroD = 2 * numero
+numeroT = 3 * numero
+numeroM = numero / 2
+
+print(f'O numero coletado foi: {numero}')
+print(f'O dobro do número é: {numeroD}')
+print(f'O triplo do número é: {numeroT}')
+print(f'A metade do número é: {numeroM}')
