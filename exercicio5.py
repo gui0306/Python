20f7ee03e7a85777d9d7c688c61b95e98d0c3b19
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-25 20:58:20 -0300

    Add measurement conversion from meters to cm and mm

---
+++ exercicio5.py
@@ -0,0 +1,10 @@
+''' Conversão de medidas '''
+
+metro = float(input("metros: "))
+
+centimetro = int(metro * 100)
+milimetro = int(metro * 1000)
+
+print(f'centimetros: {centimetro}')
+print(f'milimetros: {milimetro}')
+print(f'metros: {metro}')
