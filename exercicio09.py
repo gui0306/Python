b75bbb3562ed2808010d7b8a064d4b18549acb63
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-26 18:37:20 -0300

    Implement salary adjustment calculation

--- exercicio09.py
+++ exercicio09.py
@@ -1,1 +1,10 @@
+''' Reajuste Salarial '''
 
+print ("salario atual: ")
+ValorS = float(input())
+
+salarioaumento = ValorS * 0.15
+print (f'salário com aumento: {salarioaumento:.2f}')
+
+salarioatual = ValorS + salarioaumento
+print (f'novo salário: {salarioatual:.2f}')
