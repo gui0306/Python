9d45be980eee14af402b702865861effdb5c7a92
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-26 18:29:33 -0300

    Implement discount calculation in exercicio08.py
    Added functionality to calculate and display discount on a product price.

--- exercicio08.py
+++ exercicio08.py
@@ -1,2 +1,11 @@
 ''' Desconto no produto '''
 
+print ("digite valor: ")
+valor = float(input())
+
+print (f'preço R$: {valor:.2fr}')
+desconto = valor * 0.05
+print (f'desconto R$: {desconto:.2f}')
+
+precoFinal = valor - desconto
+print (f'preço final R$: {precoFinal:.2f}')
