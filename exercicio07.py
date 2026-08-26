0d564cdb3c3ad0d41c5bab5573f39c5451d1dbee
gui0306 <guilhermeimamura2008@gmail.com>
2026-08-25 21:20:58 -0300

    Add temperature conversion from Celsius to Fahrenheit

---
+++ exercicio07.py
@@ -0,0 +1,6 @@
+''' Celsius para Fahrenheit '''
+
+celcius = 100
+fahrenheit = (celcius * 9/5) + 32
+print(f'Temperatura em Celsius: ', celcius°C")
+print(f'Temperatura em Fahrenheit: ', int(fahrenheit)°F")
